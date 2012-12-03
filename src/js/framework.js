c3cd82e7a47d4320132670b5e357bc426c0378ec
mark.macdonald <mark.macdonald@6d382281-9f57-4272-8983-3767f908fdf5>
2012-12-03 17:37:57 +0000

    Added file upload input. Fixed radio and checkbox size in IE. Improved form layout. Fixed IE old browser warning regression.

--- src/js/framework.js
+++ src/js/framework.js
@@ -20,10 +20,10 @@ function oldBrowserWarning(){
  */
  if ($.browser.msie && $.browser.version < lastSupportedIEVersion){
   var browser_warning = new Array(), i = -1;
-  browser_warning[++i] = '<p id="browser_warning" class="warning">';
+  browser_warning[++i] = '<p id="w-browser-warning" class="w-error" style="margin:0;">';
   browser_warning[++i] = 'You are using an old version of Internet Explorer which is unsupported. Some features may not work correctly. ';
   browser_warning[++i] = 'Consider <a href="http://www.whatbrowser.org/intl/en_uk/">upgrading</a> to a modern browser. ';
-  browser_warning[++i] = '<span id="dismiss_browser-warning" class="falseLink">Dismiss.</span> ';
+  browser_warning[++i] = '<span id="w-dismiss-browser-warning" class="w-false-link">Dismiss.</span> ';
   browser_warning[++i] = '</p>';
   $('body').prepend(browser_warning.join(''));
  }
@@ -33,12 +33,41 @@ $(document).ready(function() {
 
  oldBrowserWarning();	
  
- $('body').on('click', '#dismiss_browser_warning', function(){
-  $("#browser_warning").remove();
+ /*
+  Make it possible to close the old browser warning
+ */
+ $('body').on('click', '#w-dismiss-browser-warning', function(){
+  $("#w-browser-warning").remove();
  });
- 
- $('.w-breadcrumb li:last-child a').click(function(event) { //prevent the selected breadcrumb link from being clickable
+
+ /*
+  Prevent the selected breadcrumb link from being clickable
+ */
+ $('.w-breadcrumb li:last-child a').click(function(event) {
 	event.preventDefault();
  });
- 
+
+ /*
+  Trigger the file upload dialog when the 'fake' file upload button is clicked
+ */
+ $('.w-file-input-button').click(function() {
+	$(this).prev('input[type=text]').prev('input[type=file]').trigger('click');
+ }); 
+
+ /*
+  Trigger the file upload dialog when the 'fake' file upload input is clicked
+ */
+ $('.w-file-input').click(function() {
+	$(this).prev('input[type=file]').trigger('click');
+ });
+
+ /*
+  Display the file upload result in the 'fake' file upload text input
+ */ 
+ $('input[type=file]').change(function() {
+	var fileName = $(this).val().replace(/\\/g,'/') //replace windows backslashes with forward slashes
+	fileName = fileName.split('/').pop(); //split path by forward slash and select the last segment (filename). This is done to remove "C:\fakepath\ inserted by the browser.
+	$(this).next('input[type=text]').val(fileName);
+ });
+
 });
