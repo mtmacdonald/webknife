0864a681038a421dd9d5595b7184e6d3078add75
Mark Macdonald <mark.t.macdonald@googlemail.com>
2013-03-04 14:42:18 +0100

    Refactored modal dialog to simplify styling

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,24 +4,18 @@
     init : function( options ) {
 		var windowWidth = document.documentElement.clientWidth;
 		var windowHeight = document.documentElement.clientHeight;
+		var modalTop = windowHeight/2-options.height/2;
+		var modalLeft = windowWidth/2-options.width/2;
 
 		var html = new Array(), i = -1;
 		html[++i] = '<div class="wi-modal-container"></div>';
-		html[++i] = '<div style="width:'+options.width+'px; height:'+options.height+'px;" class="wi-modal">';
+		html[++i] = '<div style="width:'+options.width+'px; height:'+options.height+'px; top:'+modalTop+'px; left:'+modalLeft+'px;" class="wi-modal">';
 		html[++i] = '<a class="wi-modal-close">x</a>';
 		html[++i] = '<h1>'+options.title+'</h1>';	
 		html[++i] = '<div class="wi-modal-content">'+options.html+'</div>';
 		html[++i] = '</div>';
-		html[++i] = '<div class="wi-modal-background"></div>';
+		html[++i] = '<div class="wi-modal-background" style="opacity: 0.2;"></div>';
 		this.append(html.join(''));
-
-		$('.wi-modal-background').css({'opacity': "0.2"});
-		$('.wi-modal-background').show();
-		$('.wi-modal').show();
-
-		var popupHeight = $('.wi-modal').height();
-		var popupWidth = $('.wi-modal').width();
-		$('.wi-modal').css({'top': windowHeight/2-popupHeight/2,'left': windowWidth/2-popupWidth/2});
 
 		return this.find('.wi-modal-close').click(onClick)
 			function onClick(){
