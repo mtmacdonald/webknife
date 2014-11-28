5d3e02d7b79ace15038bf10d2ebc7917fa0c7a38
Mark Macdonald <mark.t.macdonald@googlemail.com>
2014-11-28 12:56:34 +0100

    moved the new icon script into a gulp task

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -23,6 +23,9 @@ var uglify = require('gulp-uglify');
 //for static analysis
 var jscs = require('gulp-jscodesniffer');
 
+var fs = require('fs');
+var cheerio = require('cheerio');
+
 //config
 var config = {
     'build_directory' : '../framework/'
@@ -50,11 +53,45 @@ gulp.task('css', function() {
 });
 
 gulp.task('icon', function() {
-    gulp.src('./images/svg/*.svg')
-        .pipe(svgmin())
-        .pipe(insert.wrap("var foo='", "';"))
-        .pipe(concat('framework.icons.js'))
-        .pipe(gulp.dest(config.build_directory));
+    var svg_dir = 'images/svg/';
+    var icons = [];
+
+    fs.readdir(svg_dir, function (err, files) {
+        if (err) throw err;
+        var c=0;
+        files.forEach( function(file) {
+            fs.readFile(svg_dir+file,'utf-8', function (err, content) {
+                c++;
+                if (err) throw err;
+                $ = cheerio.load(content, { xmlMode: true });
+                var svg = $('svg');
+                var viewBox = '';
+                var image = svg.html().replace(/\r?\n|\r/g, '').replace(/\t/g, '') //remove line breaks and tabs
+                var name = file.split('.')[0].replace(/-/g, '_'); //replace hyphens with underscores
+                //console.log('<div class="w-svg-icon" data-name="'+name+'"></div>');
+                var attributes = 'viewBox="'+svg.attr('viewBox')+'"';
+                var svg = '<svg '+attributes+'>'+image+'</svg>';
+                var item = {
+                    name: name,
+                    svg: svg
+                };
+                icons[c] = item;
+                if (c >= files.length) { //finished all async file reads
+                    var result = {};
+                    icons.forEach( function(icon) {
+                        result[icon.name] = icon.svg;
+                    });
+                    //prepare file
+                    var content = "var w_icons = "+JSON.stringify(result, null, 4);
+                    //write file
+                    fs.writeFile("../framework/framework.icons.js", content, function(err) {
+                        if (err) throw err;
+                    }); 
+                }
+            });
+        });
+    });
+
 });
 
 gulp.task('js', function() {
